b9b33d8b15c1ffa857f318e79b990fc0b5a668ff
Your Name <you@example.com>
2024-11-30 19:41:32 +0200

    fix: correct CVDocument import

--- src/components/OptimizedCV.tsx
+++ src/components/OptimizedCV.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { jsPDF } from 'jspdf';
-import { CVDocument } from './CVDocument';
+import CVDocument from './CVDocument';
 
 interface OptimizedCVProps {
   content: string;
